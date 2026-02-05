e9bc1d7354b8071c8138df5bce8712a60181ed09
Blok <j.s.blok@cultureelerfgoed.nl>
2026-02-05 12:39:49 +0100

    First working version.

--- src/datacatalog_service.py
+++ src/datacatalog_service.py
@@ -1,10 +1,9 @@
-import requests
 import json
 import os
 import logging
-import typing
+import requests
 from rdflib import Graph, Literal, URIRef
-from rdflib.namespace import DCTERMS, DCAT, RDF
+from rdflib.namespace import DCTERMS, DCAT, RDF, PROV
 
 GRAPH_ID = os.getenv('GRAPH_ID', 'default')
 OUTPUT_FILE_FORMAT = os.getenv('OUTPUT_FILE_FORMAT', 'ttl')
@@ -20,39 +19,50 @@ logging.basicConfig(
     datefmt='%Y-%m-%d %H:%M:%S')
 
 # --- Save graph to file ---
-def save_graph(graph: Graph, filepath: str, format: str) -> None:
+def save_graph(graph: Graph, filepath: str, fileformat: str) -> None:
     """Save RDF graph to file."""
     graph.serialize(
-        format=format,
+        format=fileformat,
         destination=filepath,
         encoding=ENCODING,
         auto_compact=True
     )
-    logger.info(f'Saved graph to {filepath} ({os.path.getsize(filepath)} bytes)')
+    logger.info('Saved graph to %s (%s bytes)', filepath, os.path.getsize(filepath))
+
 
 # --- Get query response from URI ---
-def get_query_response(from_url: str, query: str) -> Any:
+def get_query_response(from_url: str, query: str) -> any:
     get_params = {
         'action': 'ask',
         'query': query,
         'format': 'json'
     }
     response = requests.get(from_url, params=get_params, timeout=100)
-    logger.info(f'Query response from {from_url} received')
+    logger.info('Query response from %s received', from_url)
     return json.loads(response.text)
 
 # --- Return graph from JSON dict  ---
-def parse_json_to_graph(dc_json: Dict, graph_id: str) -> Graph:
+def parse_json_to_graph(dc_json: dict, graph_id: str) -> Graph:
     graph = Graph(identifier=graph_id)
 
     for result in dc_json['query']['results']:
-            dataset_node = URIRef(dc_json['query']['results'][result]['fullurl'])
-            dataset_properties = dc_json['query']['results'][result]['printouts']
-            graph.add((dataset_node, RDF.type, DCAT.dataset))
-            graph.add((dataset_node, DCTERMS.title, Literal(dataset_properties['Naam'])))
-            graph.add((dataset_node, DCTERMS.description, Literal(dataset_properties['Omschrijving'])))
-            graph.add((dataset_node, DCAT.mediaType, Literal(dataset_properties['Dataset type'])))
-
+        dataset_node = URIRef(dc_json['query']['results'][result]['fullurl'])
+        dataset_properties = dc_json['query']['results'][result]['printouts']
+        graph.add((dataset_node, RDF.type, DCAT.dataset))
+        graph.add((dataset_node, DCTERMS.title, Literal(dataset_properties['Naam'])))
+        graph.add((dataset_node, DCTERMS.description, Literal(dataset_properties['Omschrijving'])))
+        graph.add((dataset_node, DCAT.theme, Literal(dataset_properties['Dataset type'])))
+        graph.add((dataset_node, DCTERMS.publisher, URIRef('https://www.cultureelerfgoed.nl')))
+        graph.add((dataset_node, DCAT.servesDataset, URIRef('https://linkeddata.cultureelerfgoed.nl/catalog')))
+        graph.add((dataset_node, DCAT.keyword, Literal(dataset_properties['Dataset domein'])))
+        graph.add((dataset_node, PROV.wasGeneratedBy, Literal(dataset_properties['Dataset creatie'])))
+        graph.add((dataset_node, DCAT.accessURL, Literal(dataset_properties['Bronurl'])))
+        if 'Ja' in dataset_properties['Zichtbaar in Erfgoedatlas']: 
+            graph.add((dataset_node, DCTERMS.isReferencedBy, URIRef('https://rce.webgis.nl/nl/map/erfgoedatlas')))
+        graph.add((dataset_node, DCAT.distribution, Literal(dataset_properties['Dataset'])))
+        graph.add((dataset_node, DCAT.keyword, Literal(dataset_properties['Dataset rubriek'])))
+        graph.add((dataset_node, DCTERMS.accessRights, Literal(dataset_properties['Dataset beperkingen'])))
+        graph.remove((dataset_node, None, Literal('[]')))
     return graph
 
 def main():
@@ -60,7 +70,7 @@ def main():
     #with open('datacatalog.json') as f:
     #    datacatalog_json = json.load(f)
     graph = parse_json_to_graph(datacatalog_json, GRAPH_ID)
-    save_graph(graph, TARGET_FILEPATH, OUTPUT_FILE_FORMAT)        
+    save_graph(graph, TARGET_FILEPATH, OUTPUT_FILE_FORMAT)
 
 if __name__ == '__main__':
     main()
