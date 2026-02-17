e2355993e8e479f593bfe6645682fb6e18708926
Blok <j.s.blok@cultureelerfgoed.nl>
2026-02-17 19:47:14 +0100

    debugging validation

--- src/datacatalog_service.py
+++ src/datacatalog_service.py
@@ -12,15 +12,18 @@ BASE_URI = os.getenv('BASE_URI', 'https://kennis.cultureelerfgoed.nl/api.php')
 ENCODING = os.getenv('ENCODING', 'utf-8')
 ALLOWLIST_PATH = os.getenv('ALLOWLIST_PATH', 'allowlist.jsonld')
 
-# --- Logging ---
+KB_DC_QUERY = '[[Categorie:Datasets]]|limit=500|?Status|?Batch|?Naam|?Dataset type|?Omschrijving' \
+'|?Zichtbaar in Erfgoedatlas|?Dataset|?Bronurl|?Dataset creatie|?Dataset domein|?Dataset rubriek|?Dataset beperkingen'
+VALIDATION_API = 'https://datasetregister.netwerkdigitaalerfgoed.nl/api/datasets/validate'
+
 logger = logging.getLogger(__name__)
 logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
     level=logging.INFO,
     datefmt='%Y-%m-%d %H:%M:%S')
 
-# --- Get query response from URI ---
 def get_mwquery_response_as_json(from_url: str, query: str) -> any:
+    """ Get query response from URI """
     get_params = {
         'action': 'ask',
         'query': query,
@@ -30,8 +33,9 @@ def get_mwquery_response_as_json(from_url: str, query: str) -> any:
     logger.info('Query response from %s received', from_url)
     return json.loads(response.text)
 
-# --- Return graph from JSON dict  ---
 def parse_json_to_graph(dc_json: dict, graph_id: str, allowlist: Graph) -> Graph:
+    """ Return graph from query response as JSON dict """
+
     graph = Graph(identifier=graph_id)
 
     # organization information
@@ -56,52 +60,43 @@ def parse_json_to_graph(dc_json: dict, graph_id: str, allowlist: Graph) -> Graph
     for result in dc_json['query']['results']:
         # dataset definition
         dataset_node = URIRef(dc_json['query']['results'][result]['fullurl'])
-        if (dataset_node, RDF.type, SDO.DataDownload) in allowlist:
+        dataset_properties = dc_json['query']['results'][result]['printouts']
+
+        #if any(word in 'some one long two phrase three' for word in list_):
+        if (dataset_node, RDF.type, SDO.DataDownload) in allowlist and any(word in dataset_properties['Dataset beperkingen'][0] for word in ['Nee', 'Geen']):
             logger.info('Found dataset %s in allowlist.', str(dataset_node))
-            dataset_properties = dc_json['query']['results'][result]['printouts']
-            ds_name = Literal(dataset_properties['Naam'][0], lang='nl')
-            ds_description = Literal(dataset_properties['Omschrijving'][0], lang='nl')
-            ds_genre = Literal(dataset_properties['Dataset rubriek'][0], lang='nl')
-            ds_license = URIRef('https://creativecommons.org/licenses/by/4.0/')
-            ds_domain = Literal(dataset_properties['Dataset domein'][0], lang='nl')
-            ds_encoding = Literal('application/sparql-results+xml')
-            ds_contentURL = URIRef(allowlist.value(dataset_node, SDO.contentUrl) or dataset_properties['Bronurl'][0])
-
-            if 'Nee' or 'Geen' in dataset_properties['Dataset beperkingen']:
-                graph.add((dataset_node, SDO.license, ds_license))
-            else: 
-                logger.info('No License: %s', dataset_properties['Dataset beperkingen'])
-
             graph.add((dataset_node, RDF.type, SDO.Dataset))
-            graph.add((dataset_node, SDO.name, ds_name))
+            graph.add((dataset_node, SDO.name, Literal(dataset_properties['Naam'][0], lang='nl')))
             graph.add((dataset_node, SDO.publisher, organization_node))
-            graph.add((dataset_node, SDO.description, ds_description))
-            graph.add((dataset_node, SDO.genre, ds_genre))
+            graph.add((dataset_node, SDO.description, Literal(dataset_properties['Omschrijving'][0], lang='nl')))
+            graph.add((dataset_node, SDO.genre, Literal(dataset_properties['Dataset rubriek'][0], lang='nl')))
+            graph.add((dataset_node, SDO.license, URIRef('https://creativecommons.org/licenses/by/4.0/')))
 
             if 'Alle' not in dataset_properties['Dataset domein'][0]:
-                graph.add((dataset_node, SDO.keywords, ds_domain))
+                graph.add((dataset_node, SDO.keywords, Literal(dataset_properties['Dataset domein'][0], lang='nl')))
+            else:
+                graph.add((dataset_node, SDO.keywords, Literal('Monumenten; Landschap; Kunstcollecties; Archeologie; Gebouwd; Roerend', lang='nl')))
 
             graph.remove((dataset_node, None, Literal('')))
             dl_distribution_node = URIRef(dataset_properties['Bronurl'][0])
             graph.add((dl_distribution_node, RDF.type, SDO.DataDownload))
-            graph.add((dl_distribution_node, SDO.encodingFormat, ds_encoding))
-            graph.add((dl_distribution_node, SDO.contentUrl, ds_contentURL))
+            graph.add((dl_distribution_node, SDO.encodingFormat, Literal('application/sparql-results+xml')))
+            graph.add((dl_distribution_node, SDO.contentUrl, URIRef(str(allowlist.value(dataset_node, SDO.contentUrl) or dataset_properties['Bronurl'][0]))))
             graph.add((dataset_node, SDO.distribution, dl_distribution_node))
             graph.add((datacatalog_node, SDO.dataset, dataset_node))
 
     return graph
 
 def validate(url: str, graph: Graph) -> Graph:
+    """ Validate against endpoint """
     headers = {
         'accept': 'application/ld+json',
         'Content-Type': 'application/ld+json'}
     strgraph = graph.serialize(format=OUTPUT_FILE_FORMAT)
     response = requests.post(url, headers=headers, data=strgraph, timeout=100)
-
     logger.info('Validation API <%s> response status code: %s', url, response.status_code)
     validationgraph = Graph()
     validationgraph.parse(data=response.text, format='application/ld+json')
-    logger.info('validationgraph length: %i', len(validationgraph))
 
     for subj, pred, obj in validationgraph.triples((None, RDF.type, SH.ValidationResult)):
         rpath = validationgraph.value(subj, SH.resultPath)
@@ -112,21 +107,27 @@ def validate(url: str, graph: Graph) -> Graph:
     return validationgraph
 
 def main():
+    """ main runner for workflow """
     try:
-        datacatalog_json = get_mwquery_response_as_json('https://kennis.cultureelerfgoed.nl/api.php', '[[Categorie:Datasets]]|limit=500|?Status|?Batch|?Naam|?Dataset type|?Omschrijving|?Zichtbaar in Erfgoedatlas|?Dataset|?Bronurl|?Dataset creatie|?Dataset domein|?Dataset rubriek|?Dataset beperkingen')
-        with open('kb_datacatalog.json', 'w') as file:
+        datacatalog_json = get_mwquery_response_as_json(BASE_URI, KB_DC_QUERY)
+        with open('kb_datacatalog.json', 'w', encoding=ENCODING) as file:
             json.dump(datacatalog_json, file)
         allowlist = Graph()
         allowlist.parse(ALLOWLIST_PATH)
         graph = parse_json_to_graph(datacatalog_json, GRAPH_ID, allowlist)
         graph.serialize(format=OUTPUT_FILE_FORMAT, destination=TARGET_FILEPATH, encoding=ENCODING, auto_compact=True)  
         logger.info('Saved graph to %s (%s bytes)', TARGET_FILEPATH, os.path.getsize(TARGET_FILEPATH))  
-        vgraph = validate('https://datasetregister.netwerkdigitaalerfgoed.nl/api/datasets/validate', graph)
-        if SH.Violation not in vgraph.objects(subject=SH.ValidationResult, predicate=SH.resultSeverity):
+        vgraph = validate(VALIDATION_API, graph)
+        if vgraph.subjects(predicate=SH.resultSeverity, object=SH.Violation):
+            logger.info('No Shacl violations found in datacatalog.')
             graph.serialize(format=OUTPUT_FILE_FORMAT, destination=TARGET_FILEPATH, encoding=ENCODING, auto_compact=True)  
-            logger.info('Saved graph to %s (%s bytes)', TARGET_FILEPATH, os.path.getsize(TARGET_FILEPATH))  
+            logger.info('Saved graph to %s (%s bytes)', TARGET_FILEPATH, os.path.getsize(TARGET_FILEPATH))
+        else:
+            logger.error('Invalid datacatalog, Shacl violations found.')
     except FileNotFoundError as fnfe:
         logger.warning('No allowlist found: %s', fnfe)
+    except OSError as oe:
+        logger.warning('Failed to write datacatalog from Kennisbank to file: %s', oe)
 
 if __name__ == '__main__':
     main()
