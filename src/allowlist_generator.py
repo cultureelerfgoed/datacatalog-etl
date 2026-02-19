4737af6dccad0f23aac230c9ae3db0e83d97dd45
Jonathan Blok <guanothan@gmail.com>
2026-02-19 21:42:29 +0100

    Update allowlist_generator.py

--- src/allowlist_generator.py
+++ src/allowlist_generator.py
@@ -9,11 +9,10 @@ ENCODING = os.getenv('ENCODING', 'utf-8')
 # --- Make allowlist graph  ---
 def generate_allowlist() -> Graph:
     graph = Graph()
-    # datacatalog definition
-    #graph.add((URIRef('https://kennis.cultureelerfgoed.nl/index.php/Dataset/45'), RDF.type, SDO.DataDownload))
-    #graph.add((URIRef('https://kennis.cultureelerfgoed.nl/index.php/Dataset/45'), SDO.contentUrl, Literal('https://api.linkeddata.cultureelerfgoed.nl/datasets/thesauri/cht/services/cht-jena/sparql')))
-    #graph.add((URIRef('https://kennis.cultureelerfgoed.nl/index.php/Dataset/5'), RDF.type, SDO.DataDownload))
-    #graph.add((URIRef('https://kennis.cultureelerfgoed.nl/index.php/Dataset/5'), SDO.contentUrl, Literal('https://api.linkeddata.cultureelerfgoed.nl/datasets/thesauri/archeologischbasisregister/services/archeologischbasisregister-jena/sparql')))
+    graph.add((URIRef('https://kennis.cultureelerfgoed.nl/index.php/Dataset/45'), RDF.type, SDO.DataDownload))
+    graph.add((URIRef('https://kennis.cultureelerfgoed.nl/index.php/Dataset/45'), SDO.contentUrl, Literal('https://api.linkeddata.cultureelerfgoed.nl/datasets/thesauri/cht/services/cht-jena/sparql')))
+    graph.add((URIRef('https://kennis.cultureelerfgoed.nl/index.php/Dataset/5'), RDF.type, SDO.DataDownload))
+    graph.add((URIRef('https://kennis.cultureelerfgoed.nl/index.php/Dataset/5'), SDO.contentUrl, Literal('https://api.linkeddata.cultureelerfgoed.nl/datasets/thesauri/archeologischbasisregister/services/archeologischbasisregister-jena/sparql')))
     graph.add((URIRef('https://kennis.cultureelerfgoed.nl/index.php/Dataset/147'), RDF.type, SDO.DataDownload))
     graph.add((URIRef('https://kennis.cultureelerfgoed.nl/index.php/Dataset/147'), SDO.contentUrl, Literal('https://api.linkeddata.cultureelerfgoed.nl/datasets/thesauri/referentienetwerk/sparql')))    
     return graph
